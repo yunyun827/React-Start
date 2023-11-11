e07588b41c0d99262dcef300fa0d7c3cf9c4ece4
yunyun827 <imyure@gmail.com>
2023-11-11 14:35:29 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+
+  let post = '강남 맛집';
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4 >블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>글 제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>글 제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>글 제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
